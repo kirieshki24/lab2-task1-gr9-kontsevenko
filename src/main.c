f73ce216a01db21d2ff9701fd2c23ccc0ffa4509
Alexey Kontsevenko <80715798+kirieshki24@users.noreply.github.com>
2025-03-09 16:20:47 +0000

    Добавлена функция поиска максимального числа

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-int main(void) {
-  printf("Hello World\n");
-  printf("Goodbye World\n");
-  return 0;
+/**
+ * @brief Переставляет цифры числа для получения максимального значения.
+ * @param num Исходное число.
+ * @return Максимальное число, составленное из цифр исходного числа.
+ */
+int max_number(int num) {
+  char digits[10];
+  sprintf(digits, "%d", num);
+  int len = strlen(digits);
+
+  // Сортировка цифр в порядке убывания
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (digits[i] < digits[j]) {
+        char temp = digits[i];
+        digits[i] = digits[j];
+        digits[j] = temp;
+      }
+    }
+  }
+  return atoi(digits);
 }
